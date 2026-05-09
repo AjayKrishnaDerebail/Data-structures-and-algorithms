5bb2a815a1b9290cc6402eaed46f0cdad82c7c64
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-05-09 13:13:32 +0530

    Reverse number edge cases

--- src/main/java/com/basicmath/ReverseNum.java
+++ src/main/java/com/basicmath/ReverseNum.java
@@ -3,17 +3,42 @@ package com.basicmath;
 public class ReverseNum {
 
   void main() {
-    int n = 123;
+    int n = -123;
 
     var revNum = 0;
 
-    while (n > 0){
+    while (n > 0) {
       revNum = (n % 10) + revNum * 10;
 
       n = n / 10;
     }
 
     IO.println(revNum);
+
+    IO.println(new Solution().reverse(n));
   }
 
+}
+
+class Solution {
+
+  public int reverse(long n) {
+    long revNum = 0;
+    var isNegative = false;
+
+    if (n < 0) {
+      n = n * -1L;
+      isNegative = true;
+    }
+
+    while (n > 0) {
+      revNum = (n % 10L) + revNum * 10L;
+      n = n / 10;
+    }
+
+    if(revNum > Integer.MAX_VALUE)
+      return 0;
+
+    return isNegative ? (int) revNum * -1 : (int) revNum;
+  }
 }
