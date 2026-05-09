10770450c24b70911cedd8d8b4744d9aeefea2f4
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-05-09 17:21:55 +0530

    GCD Euclid's algo

--- src/main/java/com/basicmath/gcdNum.java
+++ src/main/java/com/basicmath/gcdNum.java
@@ -4,21 +4,35 @@ public class gcdNum {
 
   void main() {
     naiveApproach(30,60);
-    optimalApproach(30,55);
+    optimalApproach(30, 60);
   }
 
   private void naiveApproach(int a, int b) {
     var min = Math.min(a, b); // a < b ? a : b;
 
-    for(var i = min; i >=1 ; i--){
-      if(a % i == 0 && b % i == 0){
+    for (var i = min; i >= 1; i--) {
+      if (a % i == 0 && b % i == 0) {
         IO.println(i);
         return;
       }
     }
   }
 
-  private void optimalApproach(int a, int b) {
+  /**
+   * Optimal Approach using Euclid's Algorithm
+   * Time Complexity: O(log(min(a, b)))
+   * Space Complexity: O(1)
+   */
+  private void optimalApproach(int x, int y) {
+
+    while(x > 0 && y > 0){
+     if (x > y)
+       x = x % y;
+     else
+       y = y % x;
+    }
+
+    IO.println(x == 0 ? y : x);
 
   }
 
