ae46450259bfc8139e7e41dc0a4ab211b48042b3
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-05-15 09:37:45 +0530

    Print primes

--- src/main/java/com/basicmath/PrintPrime.java
+++ src/main/java/com/basicmath/PrintPrime.java
@@ -16,6 +16,24 @@ public class PrintPrime {
     }
   }
 
+  public void checkPrimeEfficient(int n) {
+    boolean isPrime = true;
+
+    for (int i = 2; i * i <= n; i++) {
+      if (n % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      System.out.println(n + " is Prime");
+    } else {
+      System.out.println(n + " is not Prime");
+    }
+
+  }
+
 
   void main() {
     checkPrimeNaive(12);
@@ -23,6 +41,14 @@ public class PrintPrime {
     checkPrimeNaive(341);
     checkPrimeNaive(101);
     checkPrimeNaive(997);
+
+    IO.println();
+
+    checkPrimeEfficient(12);
+    checkPrimeEfficient(1769);
+    checkPrimeEfficient(341);
+    checkPrimeEfficient(101);
+    checkPrimeEfficient(997);
   }
 
 }
