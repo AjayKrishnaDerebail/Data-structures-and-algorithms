2926ac3f1c809fd8ad60397b487dfe81ce62b639
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-05-16 15:45:53 +0530

    Summing N

---
+++ src/main/java/com/basics/recursion/SumRec.java
@@ -0,0 +1,19 @@
+package com.basics.recursion;
+
+public class SumRec {
+
+  int sum(int n) {
+
+    if (n == 0) {
+      return 0;
+    }
+
+    return sum(n - 1) + n;
+
+  }
+
+  void main() {
+
+    IO.println(sum(10));
+  }
+}
